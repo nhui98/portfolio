8f9199533d0ac76a795c9298a0354873c245e2ed
Nathan <hui.n1998@gmail.com>
2022-02-13 15:22:24 +0000

    navbar responsive styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react"
 
 import Navbar from "./components/NavBar/NavBar"
-import Sidebar from "./components/SideBar/SideBar"
+import HomePage from "./pages/HomePage/HomePage"
 
 import "./App.css"
 
 const App = () => (
-  <div>
+  <>
     <Navbar />
-    <Sidebar />
-  </div>
+    <HomePage />
+  </>
 )
 
 export default App
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,81 @@
 import React from "react";
 
+import { FaLinkedinIn } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi"
+
 import "./NavBar.css"
 
-const Navbar = () => (
-  <nav className="navbar">
-    
-    <ul className="navbar__links">
-      <li className="opensans"><a href="about">About</a></li>
-      <div className="navbar__links-dash" />
-      <li className="opensans"><a href="skills">Skills</a></li>
-      <div className="navbar__links-dash" />
-      <li className="opensans"><a href="projects">Projects</a></li>
-      <div className="navbar__links-dash" />
-      <li className="opensans"><a href="education">Education</a></li>
-    </ul>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
 
-    <div className="navbar__vertical">
-      <div className="navbar__vertical-dash" />
-    </div>
+  return (
+  <>
+    {/* Navbar large device*/}
+    <nav className="navbar">    
+      <ul className="navbar__links">
+        <li className="opensans"><a href="#about">About</a></li>
+        <div className="navbar__links-dash" />
+        <li className="opensans"><a href="#skills">Skills</a></li>
+        <div className="navbar__links-dash" />
+        <li className="opensans"><a href="#projects">Projects</a></li>
+        <div className="navbar__links-dash" />
+        <li className="opensans"><a href="#education">Education</a></li>
+      </ul>
 
-  </nav>
-)
+      <div className="vertical">
+        <div className="navbar__vertical-dash" />
+      </div>
+    </nav>
+
+    {/* Sidebar */}
+    <aside className="sidebar navbar ">
+      <a className="sidebar-logo" href="#landing">&#35377;</a>
+
+      <div className="vertical">
+        <div className="sidebar__vertical-dash" />
+      </div>
+
+      <div className="sidebar__social">
+        <a href="/"><HiOutlineMail color="#FFF" /></a>
+        <a href="/"><FaLinkedinIn color="#FFF" /></a>
+      </div>
+    </aside>
+
+    {/* Navbar - medium to large device*/}
+    <nav className="navbar__medium">
+      <a className="navbar__medium-logo" href="#landing">&#35377;</a>
+
+      <ul className="navbar__medium-links">
+        <li className="opensans"><a href="#about">About</a></li>
+        <li className="opensans"><a href="#skills">Skills</a></li>
+        <li className="opensans"><a href="#projects">Projects</a></li>
+        <li className="opensans"><a href="#education">Education</a></li>
+      </ul>
+    </nav>
+
+    {/* Navbar - small to medium device*/}
+    <nav className="navbar__small">
+        <div className="navbar__small-logo" onClick={() => { setToggleMenu(true) }}>
+        &#35377;
+        </div>
+        
+        {toggleMenu && (
+          <div className="navbar__small-overlay slide">
+            <div className="navbar__small-close" onClick={() => { setToggleMenu(false) }}>
+              &#35377;
+            </div>
+
+            <ul className="navbar__small-links">
+              <li><a href="#about" onClick={() => { setToggleMenu(false) }}>About</a></li>
+              <li><a href="#skills" onClick={() => { setToggleMenu(false) }}>Skills</a></li>
+              <li><a href="#projects" onClick={() => { setToggleMenu(false) }}>Projects</a></li>
+              <li><a href="#education" onClick={() => { setToggleMenu(false) }}>Education</a></li>
+            </ul>
+          </div>
+        )}
+    </nav>
+  </>
+  )
+}
 
 export default Navbar
