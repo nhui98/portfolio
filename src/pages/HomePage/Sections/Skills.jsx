d17724852513768250296b5658772b1911e32962
Nathan <hui.n1998@gmail.com>
2022-02-13 23:39:01 +0000

    skills - added grid hover effect, rotating icons

--- src/pages/HomePage/Sections/Skills.jsx
+++ src/pages/HomePage/Sections/Skills.jsx
@@ -3,22 +3,63 @@ import React from "react"
 import { IoLogoHtml5, IoLogoCss3, IoLogoJavascript, IoLogoNodejs} from "react-icons/io"
 import { SiMongodb, SiReact, SiNextdotjs, SiSass} from "react-icons/si"
 
-import Header from "../../../components/Header/Header"
+
+import "./Skills.css"
+
+const Grid = () => (
+  <div className="grid-container">
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+    {/* 10 divs */}
+    <div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" /><div className="grid-item" />
+  </div>
+)
 
 const Skills = () => (
   <section className="skills" id="skills">
-    <Header subtitle="What I Use" />
-    <h1 className="skills__header">Technology</h1>
     <div className="skills__container">
-      <IoLogoHtml5 />
-      <IoLogoCss3 />
-      <SiSass />
-      <IoLogoJavascript />
-      <SiReact />
-      <SiNextdotjs />
-      <IoLogoNodejs />
-      <SiMongodb />
+      <div className="skills__container-row">
+        <div className="skills__skill html"><IoLogoHtml5 /></div>
+        <div className="skills__skill css"><IoLogoCss3 /></div>
+        <div className="skills__skill sass"><SiSass /></div>
+      </div>
+
+      <div className="skills__container-row">
+        <div className="skills__skill js"><IoLogoJavascript /></div>
+        <div className="skills__skill react"><SiReact /></div>
+      </div>
+      
+      <div className="skills__container-row">
+        <div className="skills__skill next"><SiNextdotjs /></div>
+        <div className="skills__skill node"><IoLogoNodejs /></div>
+        <div className="skills__skill mongo"><SiMongodb /></div>
+      </div>
     </div>
+    <Grid />
   </section>
 )
 
